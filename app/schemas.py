4d19b946d7995576ff596e479287402bf93d935f
Zeyad Essam <72177003+zezo-rgb@users.noreply.github.com>
2023-08-11 00:55:46 +0300

    Adding User-Schemas

---
+++ app/schemas.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+
+class PostBase(BaseModel):
+    title: str 
+    content: str
+    published: bool = True #default true
+
+
+class PostCreate(PostBase): 
+    pass 
+
+
+class PostResponse(PostBase):
+    id: int
+    created_at: datetime
+
+class Config:
+    orm_mode = True
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(PostBase):
+    id: int
+    email: EmailStr
+    
+
+
+    class Config:
+        orm_mode = True
